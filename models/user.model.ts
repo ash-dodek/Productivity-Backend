057f02f22a0555e0b9057ebdb710b03a8b4a6530
Harsh Tripathi <harshtrip2007@gmail.com>
2025-08-02 14:37:20 +0530

    Updating methods in user model

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,4 +1,6 @@
 import mongoose, { Document, InferSchemaType, model, Schema } from "mongoose"
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken"
 
 const UserSchema = new Schema({
     name: {
@@ -30,4 +32,32 @@ const UserSchema = new Schema({
 
 type userType = InferSchemaType<typeof UserSchema>
 
-export const UserModel = model<userType>("User", UserSchema)
+interface IUserWithMethods extends userType {
+    generateRefreshToken(): string | null;
+}
+
+UserSchema.pre('save', async function (next) {
+    if(!this.isModified('password')) return next()
+
+    this.password = await bcrypt.hash(this.password, 9)
+    return next()
+})
+
+UserSchema.methods.generateRefreshToken = async function() {
+    try{
+        return await jwt.sign(
+            {
+                userId: this._id.toString(),
+            },
+            process.env.JWT_SECRET_RF!
+        )
+        
+    }catch(error){
+        console.error(error)
+    }
+}
+
+
+
+
+export const UserModel = model<IUserWithMethods>("User", UserSchema)
